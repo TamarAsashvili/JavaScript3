f0ff00ad4ba0c0ee94d02b9bc712f68f28b372a9
TamarAsashvili <47082054+TamarAsashvili@users.noreply.github.com>
2019-04-02 22:01:06 +0200

    Add files via upload

--- homework/index.js
+++ homework/index.js
@@ -1,47 +1,64 @@
-'use strict';
+// Step 0: Here's a bar class and object you can use.
 
-{
-  function fetchJSON(url, cb) {
-    const xhr = new XMLHttpRequest();
-    xhr.open('GET', url);
-    xhr.responseType = 'json';
-    xhr.onload = () => {
-      if (xhr.status < 400) {
-        cb(null, xhr.response);
-      } else {
-        cb(new Error(`Network error: ${xhr.status} - ${xhr.statusText}`));
-      }
-    };
-    xhr.onerror = () => cb(new Error('Network request failed'));
-    xhr.send();
-  }
-
-  function createAndAppend(name, parent, options = {}) {
-    const elem = document.createElement(name);
-    parent.appendChild(elem);
-    Object.keys(options).forEach(key => {
-      const value = options[key];
-      if (key === 'text') {
-        elem.textContent = value;
-      } else {
-        elem.setAttribute(key, value);
-      }
-    });
-    return elem;
-  }
-
-  function main(url) {
-    fetchJSON(url, (err, data) => {
-      const root = document.getElementById('root');
-      if (err) {
-        createAndAppend('div', root, { text: err.message, class: 'alert-error' });
-      } else {
-        createAndAppend('pre', root, { text: JSON.stringify(data, null, 2) });
-      }
-    });
-  }
-
-  const HYF_REPOS_URL = 'https://api.github.com/orgs/HackYourFuture/repos?per_page=100';
-
-  window.onload = () => main(HYF_REPOS_URL);
+class Bar {
+    serveDrink(drink) {
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(drink), 1000);
+        })
+    }
 }
+
+const favoriteBar = new Bar();
+
+// Step 1: declare a class 'Person'.
+
+
+class Person {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    greet() {
+        let name = 'Mariam';
+        console.log(`Hi ${this.name}, I am Tamar.`);
+    }
+
+    drink() {
+        console.log(this.toString());
+    }
+
+    async getDrink(drink) {
+        return new Promise((resolve) => {
+            setTimeout(() => resolve(drink), 1000);
+        })
+    }
+}
+
+const newPerson = new Person();
+
+
+
+  // A person has 3 properties: a name, an age, and a favorite drink (as just a string).
+
+  // They are all required, and must be specified when creating the person.
+
+  // A person has 2 synchronous functions, and one asynchronous one.
+
+  // Sync: 
+  // - greet. You greet another person. You say  his/her name and your own name, in the console.
+// - drink. You drink a drink (which is just a string), in the console.
+// Async:
+// - getDrink. You go and prepare a drink (just a string) from the bar; this takes a while.
+
+// Step 2: create 2 Person instances: yourself, and your favorite waiter or waitress.
+
+
+
+// Step 3: Say hi to your favorite waiter/waitress.
+
+
+
+// Step 4: The waiter/waitress knows you well, and immediately goes 
+// and fetch your favorite drink from the bar.
+// Then, when it arrives, drink it!
